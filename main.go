7a7550d976f84ce40b3b0abc0959e8048b82a678
pekseneren <peksen.eren@gmail.com>
2021-03-16 13:54:13 +0000

    Added repository name as a arg

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	sendMessage()
+	repositoryName := os.Args[1]
+
+	sendMessage(repositoryName)
 }
 
-func sendMessage() {
+func sendMessage(repositoryName string) {
 
 	token := os.Getenv("BOT_TOKEN")
 	chatid := os.Getenv("CHAT_ID")
 
-	resp, err := http.Post("https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chatid+"&text=A push/pullrequest has been commited to main branch", "application/json", nil)
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=A push/pullrequest has been commited to your (%s) repository on main branch", token, chatid, repositoryName)
+
+	resp, err := http.Post(url, "application/json", nil)
 
 	if err != nil {
 		log.Fatal(err, resp)
